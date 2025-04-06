0ec935564cec243219a27cae0c5b7e089ec818bb
Noureldin <noureeldinhossamm@gmail.com>
2025-04-06 11:36:00 +0200

    Make some updates

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
