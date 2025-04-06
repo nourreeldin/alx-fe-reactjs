73b3a124692cf59e117e7982b935ccb35e9d3260
Noureldin <noureeldinhossamm@gmail.com>
2025-04-06 12:27:17 +0200

    Make a new dir

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: 'gray', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e3e3e3', textAlign: 'center' }}>
+      <p style={{ fontSize: '20px' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
