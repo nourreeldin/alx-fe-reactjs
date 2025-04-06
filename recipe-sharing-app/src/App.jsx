f0e7621a7174ca7c4a1930bbcd1a05ba53b4c4f8
Noureldin <noureeldinhossamm@gmail.com>
2025-04-06 19:28:54 +0200

    Make sth amazing!

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <h2>Recipes</h2>
+      <RecipeList />
+    </div>
+  );
+};
+
+export default App;
