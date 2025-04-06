23ecea3e6faa1aa94ed8474d52483e5c52b5a586
Noureldin <noureeldinhossamm@gmail.com>
2025-04-06 19:14:16 +0200

    Make updates

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,27 +1,20 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com'; 
-
+const BASE_URL = 'https://api.github.com/search/users';
 
 export const fetchUserData = async (query, location = '', minRepos = 0) => {
-
   let searchQuery = `${query}`;
-
 
   if (location) {
     searchQuery += ` location:${location}`;
   }
 
-
   if (minRepos) {
     searchQuery += ` repos:>=${minRepos}`;
   }
 
-
   try {
-    const response = await axios.get(`${BASE_URL}/search/users`, {
-      params: { q: searchQuery },
-    });
+    const response = await axios.get(`${BASE_URL}?q=${searchQuery}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching user data:', error);
